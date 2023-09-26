f33f549e2eccb4b3c9759cb70fcf65fad5669e7f
Simon Sundberg <simon.sundberg@iths.se>
2023-09-26 10:41:22 +0200

    Add project files.

---
+++ Car Rental.App/Client/Program.cs
@@ -0,0 +1,20 @@
+using Car_Rental.App;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace Car_Rental.App
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
