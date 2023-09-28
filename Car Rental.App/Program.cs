1e8b2d73a0efcb4d0257a76a1219572de78405c9
Simon Sundberg <simon.sundberg@iths.se>
2023-09-28 10:56:40 +0200

    Make App use top level statements

---
+++ Car Rental.App/Program.cs
@@ -0,0 +1,16 @@
+using Car_Rental.App;
+using Car_Rental.Business.Classes;
+using Car_Rental.Data.Classes;
+using Car_Rental.Data.Interfaces;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton<IData, CollectionData>();
+builder.Services.AddSingleton<BookingProcessor>();
+
+await builder.Build().RunAsync();
