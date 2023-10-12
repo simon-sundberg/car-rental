5d1a4f017041fcfae061e0bfbac06d4b05320e55
Simon Sundberg <simon.sundberg@iths.se>
2023-10-12 11:35:18 +0200

    Refactoring of add vehicle

--- Car Rental.Business/Classes/BookingProcessor.cs
+++ Car Rental.Business/Classes/BookingProcessor.cs
@@ -5,49 +5,27 @@ namespace Car_Rental.Business.Classes;
 public class BookingProcessor
 {
     readonly IData _db;
-    public AddVehicleForm AddVehicleData = new();
+    public AddVehicleForm AddVehicleForm = new();
+    public bool AddVehicleButtonWasClicked { get; private set; }
     public BookingProcessor(IData db) => _db = db;
     public List<IVehicle> GetVehicles() => _db.GetVehicles();
     public List<IVehicle> GetVehicles(VehicleStatuses status) => _db.GetVehicles(status);
     public List<IPerson> GetCustomers() => _db.GetCustomers();
     public List<IBooking> GetBookings() => _db.GetBookings();
-
-    public bool DisplayAddVehicleErrors { get; private set; } = false;
     public void OnAddVehicleSubmit()
     {
-        var data = AddVehicleData;
-        if (GetAddVehicleFormErrors().Count == 0)
+        AddVehicleForm form = AddVehicleForm;
+        if (form.GetValidationErrors().Count > 0)
         {
-            _db.AddVehicle(
-                data.RegNo ?? throw new ArgumentNullException(nameof(data.Odometer)),
-                data.Make ?? throw new ArgumentNullException(nameof(data.Make)),
-                data.Odometer ?? throw new ArgumentNullException(nameof(data.Odometer)),
-                data.CostKm ?? throw new ArgumentNullException(nameof(data.CostKm)),
-                data.Type ?? throw new ArgumentNullException(nameof(data.Type)),
-                data.CostDay ?? throw new ArgumentNullException(nameof(data.CostDay))
-                );
+            AddVehicleButtonWasClicked = true;
+            return;
         }
-        else
-        {
-            DisplayAddVehicleErrors = true;
-        }
-    }
-    public List<string> GetAddVehicleFormErrors()
-    {
-        var data = AddVehicleData;
-        List<string> errors = new();
-        if (data.RegNo?.Length != 6)
-            errors.Add("RegNo must be 6 characters long");
-        if (!(data.Make?.Length > 0))
-            errors.Add("Make is required");
-        if (!(data.Odometer >= 0))
-            errors.Add("Odometer must be a positive number");
-        if (!(data.CostKm >= 0))
-            errors.Add("Cost / Km must be a positive number");
-        if (data.Type is null)
-            errors.Add("Type is required");
-        if (!(data.CostDay >= 0))
-            errors.Add("Cost / Day must be a positive number");
-        return errors;
+        _db.AddVehicle(form.RegNo ?? throw new ArgumentNullException(nameof(form.Odometer)),
+                       form.Make ?? throw new ArgumentNullException(nameof(form.Make)),
+                       form.Odometer ?? throw new ArgumentNullException(nameof(form.Odometer)),
+                       form.CostKm ?? throw new ArgumentNullException(nameof(form.CostKm)),
+                       form.Type ?? throw new ArgumentNullException(nameof(form.Type)),
+                       form.CostDay ?? throw new ArgumentNullException(nameof(form.CostDay)));
+        AddVehicleButtonWasClicked = false;
     }
 }
