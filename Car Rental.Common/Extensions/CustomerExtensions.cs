1c3eb56cb98445bd27968288905de4db68f2ef12
Simon Sundberg <simon.o.sundberg@gmail.com>
2023-10-17 17:41:44 +0200

    Move validation to extensions

---
+++ Car Rental.Common/Extensions/CustomerExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Car_Rental.Common.Interfaces;
+
+namespace Car_Rental.Common.Extensions;
+
+public static class CustomerExtensions
+{
+    public static List<string> GetErrors(this IPerson customer)
+    {
+        List<string> errors = new();
+        string cleanedSSN = customer.SSN.Replace(" ", "").Replace("-", "");
+        if (cleanedSSN.Length != 12 || !double.TryParse(cleanedSSN, out _))
+            errors.Add("SSN must contain 12 digits");
+        if (!(customer.LastName?.Length > 0))
+            errors.Add("Last Name is required");
+        if (!(customer.FirstName?.Length > 0))
+            errors.Add("First Name is required");
+        return errors;
+    }
+}
--- Car Rental.Common/Extensions/VehicleExtensions.cs
+++ Car Rental.Common/Extensions/VehicleExtensions.cs
@@ -1,7 +1,24 @@
-﻿namespace Car_Rental.Common.Extensions;
+﻿using Car_Rental.Common.Classes;
+
+namespace Car_Rental.Common.Extensions;
 
 public static class VehicleExtensions
 {
     public static int Duration(this DateOnly startDate, DateOnly endDate)
         => endDate.DayNumber - startDate.DayNumber;
+    public static List<string> GetErrors(this Vehicle vehicle)
+    {
+        List<string> errors = new();
+        if (vehicle.RegNo.Length != 6)
+            errors.Add("RegNo must be 6 characters long");
+        if (vehicle.Make.Length == 0)
+            errors.Add("Make is required");
+        if (vehicle.Odometer <= 0)
+            errors.Add("Odometer must be a positive number");
+        if (vehicle.CostKm <= 0)
+            errors.Add("Cost / Km must be a positive number");
+        if (vehicle.CostDay <= 0)
+            errors.Add("Cost / Day must be a positive number");
+        return errors;
+    }
 }
