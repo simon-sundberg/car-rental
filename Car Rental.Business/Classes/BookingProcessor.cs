559ac066dcbd9878e91e3ca558e5483407d5a7dc
Simon Sundberg <simon.o.sundberg@gmail.com>
2023-10-18 20:49:52 +0200

    Refactor BookingProcessor

--- Car Rental.Business/Classes/BookingProcessor.cs
+++ Car Rental.Business/Classes/BookingProcessor.cs
@@ -7,23 +7,51 @@ namespace Car_Rental.Business.Classes;
 public class BookingProcessor
 {
     readonly IData _db;
-
-    public Customer customerForm = new();
-    public Vehicle vehicleForm = new();
-    public string? rentingCustomerSSN = null;
-    public int? returnDistance = null;
-    public bool Processing = false;
+    public BookingProcessor(IData db) => _db = db;
+    public Customer CustomerForm = new();
+    public Vehicle VehicleForm = new();
+    public string? RentingCustomerSSN;
+    public int? ReturnDistance;
+    public bool Processing;
     public bool AddCustomerButtonWasClicked { get; private set; }
     public bool AddVehicleButtonWasClicked { get; private set; }
-    public BookingProcessor(IData db) => _db = db;
-    public List<Vehicle> GetVehicles() => _db.GetVehicles();
-    public List<Vehicle> GetVehicles(VehicleStatuses status) => _db.GetVehicles(status);
-    public List<IPerson> GetCustomers() => _db.GetCustomers();
-    public List<IBooking> GetBookings() => _db.GetBookings();
-    public List<String> CustomerFormErrors => customerForm.GetErrors();
-    public List<String> VehicleFormErrors => vehicleForm.GetErrors();
+    public List<String> CustomerFormErrors => CustomerForm.GetErrors();
+    public List<String> VehicleFormErrors => VehicleForm.GetErrors();
     public string[] VehicleStatusNames => _db.VehicleStatusNames;
     public string[] VehicleTypeNames => _db.VehicleTypeNames;
+    public List<IBooking> GetBookings() => _db.GetBookings();
+    public List<IPerson> GetCustomers() => _db.GetCustomers();
+    public List<Vehicle> GetVehicles() => _db.GetVehicles();
+    public List<Vehicle> GetVehicles(VehicleStatuses status) => _db.GetVehicles(status);
+    public void AddCustomer()
+    {
+        Customer form = CustomerForm;
+        if (CustomerForm.GetErrors().Count > 0)
+        {
+            AddCustomerButtonWasClicked = true;
+            return;
+        }
+        _db.AddCustomer(form.SSN.Length > 0 ? form.SSN : throw new ArgumentException(nameof(form.SSN)),
+                        form.LastName.Length > 0 ? form.LastName : throw new ArgumentException(nameof(form.LastName)),
+                        form.FirstName.Length > 0 ? form.FirstName : throw new ArgumentException(nameof(form.FirstName)));
+        AddCustomerButtonWasClicked = false;
+    }
+    public void AddVehicle()
+    {
+        Vehicle form = VehicleForm;
+        if (VehicleForm.GetErrors().Count > 0)
+        {
+            AddVehicleButtonWasClicked = true;
+            return;
+        }
+        _db.AddVehicle(form.RegNo.Length > 0 ? form.RegNo : throw new ArgumentException(nameof(form.Odometer)),
+                       form.Make.Length > 0 ? form.Make : throw new ArgumentException(nameof(form.Make)),
+                       form.Odometer > 0 ? form.Odometer : throw new ArgumentException(nameof(form.Odometer)),
+                       form.CostKm > 0 ? form.CostKm : throw new ArgumentException(nameof(form.CostKm)),
+                       form.Type ?? throw new ArgumentNullException(nameof(form.Type)),
+                       form.CostDay > 0 ? form.CostDay : throw new ArgumentException(nameof(form.CostDay)));
+        AddVehicleButtonWasClicked = false;
+    }
     public async Task RentVehicle(Vehicle vehicle, string customerSSN)
     {
         IPerson customer = _db.GetCustomer(customerSSN) ?? throw new ArgumentException("Didn't find a customer with the provided SSN.");
@@ -40,34 +68,5 @@ public class BookingProcessor
             throw new ArgumentOutOfRangeException(nameof(kmDistance));
         IBooking booking = _db.GetBooking(vehicle.RegNo) ?? throw new ArgumentException("Didn't find a booking for the provided vehicle regNo.");
         booking.ReturnVehicle((int)kmDistance);
-    }
-    public void AddCustomer()
-    {
-        Customer form = customerForm;
-        if (customerForm.GetErrors().Count > 0)
-        {
-            AddCustomerButtonWasClicked = true;
-            return;
-        }
-        _db.AddCustomer(form.SSN.Length > 0 ? form.SSN : throw new ArgumentException(nameof(form.SSN)),
-                        form.LastName.Length > 0 ? form.LastName : throw new ArgumentException(nameof(form.LastName)),
-                        form.FirstName.Length > 0 ? form.FirstName : throw new ArgumentException(nameof(form.FirstName)));
-        AddCustomerButtonWasClicked = false;
-    }
-    public void AddVehicle()
-    {
-        Vehicle form = vehicleForm;
-        if (vehicleForm.GetErrors().Count > 0)
-        {
-            AddVehicleButtonWasClicked = true;
-            return;
-        }
-        _db.AddVehicle(form.RegNo.Length > 0 ? form.RegNo : throw new ArgumentException(nameof(form.Odometer)),
-                       form.Make.Length > 0 ? form.Make : throw new ArgumentException(nameof(form.Make)),
-                       form.Odometer > 0 ? form.Odometer : throw new ArgumentException(nameof(form.Odometer)),
-                       form.CostKm > 0 ? form.CostKm : throw new ArgumentException(nameof(form.CostKm)),
-                       form.Type ?? throw new ArgumentNullException(nameof(form.Type)),
-                       form.CostDay > 0 ? form.CostDay : throw new ArgumentException(nameof(form.CostDay)));
-        AddVehicleButtonWasClicked = false;
     }
 }
